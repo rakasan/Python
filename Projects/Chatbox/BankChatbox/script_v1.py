bdd7847f9d31bd8054718f3e42df04fb37e69e99
rakasan <racasan.vlad@gmail.com>
2021-03-16 21:22:07 +0200

    Added some chatbots

---
+++ Projects/Chatbox/BankChatbox/script_v1.py
@@ -0,0 +1,24 @@
+import re
+import random
+
+class SupportBot:
+  negative_responses = ("nothing", "don't", "stop", "sorry")
+
+  exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later")
+
+  def __init__(self):
+    pass
+
+  def welcome(self):
+    name = input("Hi, I'm a customer support representative. Welcome to Codecademy Bank. Before we can help you, I need some information from you. What is your first name and last name?  ")
+    will_help = input("Okay "+name +", what can I help you with?").format(name)
+    if will_help in self.negative_responses:
+      print("Okay, have a great day!")
+      return
+    else:
+      return will_help
+
+# Create a SupportBot instance
+SupportConversation = SupportBot()
+# Call the .welcome() method
+SupportConversation.welcome()
